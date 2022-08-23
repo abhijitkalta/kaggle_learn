22052e375a99de8669902ed771e87147d59134f1
Abhijit Kalta <abhijitkalta@gmail.com>
2022-08-23 21:31:24 +0530

    Project MNIST

---
+++ project_mnist/src/train.py
@@ -0,0 +1,47 @@
+import os
+import config
+import argparse
+import joblib
+import pandas as pd
+from sklearn import metrics
+from sklearn import tree
+
+def run(fold):
+    df = pd.read_csv(config.TRAINING_FILE)
+    
+    df_train = df[df.kfold != fold].reset_index(drop = True)
+    df_valid = df[df.kfold == fold].reset_index(drop = True)
+    
+    x_train = df_train.drop('target', axis = 1).values
+    y_train = df_train['target'].values
+    
+    x_valid = df_valid.drop('target', axis = 1).values
+    y_valid = df_valid['target'].values
+    
+    clf = tree.DecisionTreeClassifier()
+    
+    clf.fit(x_train, y_train)
+    
+    y_preds = clf.predict(x_valid)
+    
+    accuracy = metrics.accuracy_score(y_valid, y_preds)
+    print(f"Fold = {fold}, Accuracy = {accuracy}")
+    
+    joblib.dump(
+        clf, 
+        os.path.join(config.MODEL_OUTPUT, f"dt_{fold}.bin")
+    )
+    
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument(
+        '--fold',
+        type = int
+    )
+    
+    args = parser.parse_args()
+    
+    run(fold=args.fold)
+    
